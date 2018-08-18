b75f7fb4d2614972b8f2f214f0fc265e661dda3a
captproton <carl@wdwhub.net>
2018-08-18 11:39:01 -0700

    ADDED: static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  # match "auth/:provider/callback" => "users#create"
+  # match "login" => "users#login", :as => "login"
+  # match "logout" => "users#logout", :as => "logout"
+  # match "feedback" => "feedback_messages#new", :as => "feedback"
+  # match "episodes/archive" => redirect("/?view=list")
+  # match 'unsubscribe/:token' => 'users#unsubscribe', :as => "unsubscribe"
+  # post "versions/:id/revert" => "versions#revert", :as => "revert_version"
+
+  # resources :users do
+  #   member { put :ban }
+  # end
+  # resources :comments
   resources :episodes
+  # resources :feedback_messages
+
+  # match "tags/:id" => redirect("/?tag_id=%{id}")
+
 end
